69d4ff1d52788cc67347ba0c6a595d0c0360fa04
Laion Corcino <laioncorcino@gmail.com>
2024-02-24 19:41:28 -0300

    [feat] finalizing controller and service of doctor

--- src/main/java/com/corcino/medical/api/DoctorController.java
+++ src/main/java/com/corcino/medical/api/DoctorController.java
@@ -2,18 +2,21 @@ package com.corcino.medical.api;
 
 import com.corcino.medical.entity.Doctor;
 import com.corcino.medical.json.DoctorRequest;
+import com.corcino.medical.json.DoctorResponse;
+import com.corcino.medical.json.DoctorResponseList;
 import com.corcino.medical.service.DoctorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
+
+import static org.springframework.data.domain.Sort.Direction.ASC;
 
 @RestController
 @RequestMapping("/api/v1/doctors")
@@ -27,42 +30,27 @@ public class DoctorController {
     }
 
     @PostMapping()
-    public ResponseEntity<String> create(@RequestBody @Valid DoctorRequest doctorRequest, UriComponentsBuilder uriBuilder) throws UnsupportedEncodingException {
+    public ResponseEntity<String> create(@RequestBody @Valid DoctorRequest doctorRequest, UriComponentsBuilder uriBuilder) {
         Doctor doctor = doctorService.createDoctor(doctorRequest);
         URI uri = uriBuilder.path("/api/v1/doctors/{doctorId}").buildAndExpand(doctor.getDoctorId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
+    @GetMapping
+    public ResponseEntity<Page<DoctorResponseList>> list(@PageableDefault(sort = "doctorId", direction = ASC) Pageable pageable) {
+        return ResponseEntity.ok(doctorService.getAllDoctors(pageable));
+    }
+
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<DoctorResponse> getById(@PathVariable Long doctorId) {
+        return ResponseEntity.ok(doctorService.getDoctorById(doctorId));
+    }
+
+    @DeleteMapping("/{doctorId}")
+    public ResponseEntity<Void> delete(@PathVariable Long doctorId) {
+        doctorService.deleteDoctor(doctorId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- src/main/java/com/corcino/medical/service/DoctorService.java
+++ src/main/java/com/corcino/medical/service/DoctorService.java
@@ -1,27 +1,86 @@
 package com.corcino.medical.service;
 
+import com.corcino.medical.entity.Address;
 import com.corcino.medical.entity.Doctor;
 import com.corcino.medical.error.exception.BadRequestException;
+import com.corcino.medical.error.exception.NotFoundException;
 import com.corcino.medical.json.DoctorRequest;
+import com.corcino.medical.json.DoctorResponse;
+import com.corcino.medical.json.DoctorResponseList;
 import com.corcino.medical.repository.DoctorRepository;
+import io.micrometer.common.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class DoctorService {
 
     private final DoctorRepository doctorRepository;
+    private final AddressService addressService;
 
     @Autowired
-    public DoctorService(DoctorRepository doctorRepository) {
+    public DoctorService(DoctorRepository doctorRepository, AddressService addressService) {
         this.doctorRepository = doctorRepository;
+        this.addressService = addressService;
     }
 
     public Doctor createDoctor(DoctorRequest doctorRequest) {
         return saveDoctor(new Doctor(doctorRequest));
+    }
+
+    public DoctorResponse updateDoctor(DoctorRequest doctorRequest, Long doctorId) {
+        Doctor doctor = getById(doctorId);
+
+        if (StringUtils.isNotBlank(doctorRequest.getName())) {
+            doctor.setName(doctorRequest.getName());
+        }
+
+        if (StringUtils.isNotBlank(doctorRequest.getEmail())) {
+            doctor.setEmail(doctorRequest.getEmail());
+        }
+
+        if (StringUtils.isNotBlank(doctorRequest.getCrm())) {
+            doctor.setCrm(doctorRequest.getCrm());
+        }
+
+        if (StringUtils.isNotBlank(doctorRequest.getPhone())) {
+            doctor.setPhone(doctorRequest.getPhone());
+        }
+
+        Address address = addressService.updateAddress(doctorRequest);
+
+        if (address != null) {
+            doctor.setAddress(address);
+        }
+
+        log.info("Updating doctor " + doctor.getDoctorId());
+
+        Doctor savedDoctor = saveDoctor(doctor);
+        return new DoctorResponse(savedDoctor);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<DoctorResponseList> getAllDoctors(Pageable pageable) {
+        Page<Doctor> doctors = doctorRepository.findAll(pageable);
+        return doctors.map(DoctorResponseList::new);
+    }
+
+    public DoctorResponse getDoctorById(Long doctorId) {
+        return new DoctorResponse(getById(doctorId));
+    }
+
+    public void deleteDoctor(Long doctorId) {
+        getById(doctorId);
+        log.info("Deleting doctor of id {}", doctorId);
+        doctorRepository.deleteById(doctorId);
     }
 
     private Doctor saveDoctor(Doctor doctor) {
@@ -37,6 +96,16 @@ public class DoctorService {
             log.error("There was error saving doctor");
             throw new BadRequestException("Unknown error");
         }
+    }
+
+    private Doctor getById(Long doctorId) {
+        log.info("Finding doctor of id {}", doctorId);
+        Optional<Doctor> doctor = doctorRepository.findById(doctorId);
+
+        return doctor.orElseThrow(() -> {
+            log.error("Doctor of id {} not found", doctorId);
+            return new NotFoundException("Doctor not found");
+        });
     }
 
 }
